6741a89cd22669571e92ab35bcdeddd079a756e9
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-06-23 23:52:56 -0300

    first commit

---
+++ war-directory/src/main/java/com/thebeast/theglory/ThegloryApplication.java
@@ -0,0 +1,13 @@
+package com.thebeast.theglory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThegloryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThegloryApplication.class, args);
+	}
+
+}
---
+++ war-directory/src/test/java/com/thebeast/theglory/ThegloryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebeast.theglory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThegloryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
