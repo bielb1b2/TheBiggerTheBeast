be431f4e73bac63eb3f2ec942a704633b8a0377f
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-06-24 22:37:55 -0300

    feat: adding both projects

---
+++ thelib/src/main/java/com/thefake/thelib/HelloControllerWrapper.java
@@ -0,0 +1,11 @@
+package com.thefake.thelib;
+
+public class HelloControllerWrapper {
+
+    public HelloControllerWrapper() {
+    }
+
+    public static String sayHello() {
+        return "Hello i'm the lib";
+    }
+}
--- war-directory/src/main/java/com/thebeast/theglory/HelloController.java
+++ war-directory/src/main/java/com/thebeast/theglory/HelloController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thefake.thelib.HelloControllerWrapper;
+
 @RestController
 @RequestMapping("/")
 public class HelloController {
 
     @GetMapping("/")
-    public String helloWorld() {
-        return "<h1>Hello Mundao</h1>";
+    public String helloJarLib() {
+        return HelloControllerWrapper.sayHello();
     }
 }
